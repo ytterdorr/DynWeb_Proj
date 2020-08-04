74989d2a8f7a680a38b64faba520e04fca6e76f1
Tore Haglund <tore.haglund@gmail.com>
2020-08-04 16:43:11 +0200

    left side from js

---
+++ js/left-aside.js
@@ -0,0 +1,19 @@
+let leftSide = document.querySelector(".left-side");
+console.log(leftSide);
+
+leftSide.innerHTML = `
+<nav>
+  <ul>
+    <a href="/index.html"><li>Hem</li></a>
+    <a href="/html/gasthamn.html"><li>Gästhamn</li></a>
+    <a href="/html/kontakt.html"><li>Kontakt</li></a>
+    <a href="/html/turism.html"><li>Turism</li></a>
+    <a href="/html/historia.html"><li>Historia</li> </a>
+    <a href="/html/medlem.html"><li>Medlem</li> </a>
+    <a href="/html/english.html"><li>English</li></a>
+  </ul>
+</nav>
+<footer>
+  Is it OK to call this footer?
+</footer>
+`;
