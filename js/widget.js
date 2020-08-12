cebe454312685c338e85e3d21a2ecff35b273b43
Tore Haglund <tore.haglund@gmail.com>
2020-08-12 21:43:41 +0200

    Sky values in separate module

--- js/widget.js
+++ js/widget.js
@@ -1,38 +1,10 @@
+import SKY_VALUES from "./skyValues.js";
+
 let widget = document.querySelector("#widget");
 console.log("widget:", widget);
 if (widget == null) {
   widget = document.querySelector(".side-widget");
 }
-
-let skyValues = {
-  1: "Clear sky",
-  2: "Nearly clear sky",
-  3: "Variable cloudiness",
-  4: "Halfclear sky",
-  5: "Cloudy sky",
-  6: "Overcast",
-  7: "Fog",
-  8: "Light rain showers",
-  9: "Moderate rain showers",
-  10: "Heavy rain showers",
-  11: "Thunderstorm",
-  12: "Light sleet showers",
-  13: "Moderate sleet showers",
-  14: "Heavy sleet showers",
-  15: "Light snow showers",
-  16: "Moderate snow showers",
-  17: "Heavy snow showers",
-  18: "Light rain",
-  19: "Moderate rain",
-  20: "Heavy rain",
-  21: "Thunder",
-  22: "Light sleet",
-  23: "Moderate sleet",
-  24: "Heavy sleet",
-  25: "Light snowfall",
-  26: "Moderate snowfall",
-  27: "Heavy snowfall",
-};
 
 // Display dummy data.
 
@@ -63,14 +35,14 @@ function bulidRow(timeRow) {
     `kl: ${kl}: ${temp}, vind: ${vindriktning} (${vindstyrka}), himmel: ${himmel}`
   );
 
-  vindTD = createEl("td");
+  let vindTD = createEl("td");
 
   // Create wind direction arrow
-  vindSVG = svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  let vindSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   vindSVG.setAttribute("width", "1.1rem");
   vindSVG.setAttribute("height", "1.1rem");
 
-  var svgimg = document.createElementNS("http://www.w3.org/2000/svg", "image");
+  let svgimg = document.createElementNS("http://www.w3.org/2000/svg", "image");
   svgimg.setAttributeNS(null, "height", "1.1rem");
   svgimg.setAttributeNS(null, "width", "1.1rem");
   svgimg.setAttributeNS(
@@ -95,7 +67,7 @@ function bulidRow(timeRow) {
   row.appendChild(Object.assign(createEl("td"), { innerHTML: temp }));
   row.appendChild(vindTD);
   row.appendChild(
-    Object.assign(createEl("td"), { innerHTML: skyValues[himmel] })
+    Object.assign(createEl("td"), { innerHTML: SKY_VALUES[himmel] })
   );
   return row;
 }
@@ -126,6 +98,7 @@ function createWeatherTable(times) {
 
 function buildWidget(todayTimes, tomorrowTimes) {
   // Create Headings
+  const heading = Object.assign(createEl("h2"), { innerHTML: "Väderprognos" });
   const todayHeading = createEl("h3");
   todayHeading.innerHTML = "Idag";
   const tomorrowHeading = createEl("h3");
@@ -136,6 +109,7 @@ function buildWidget(todayTimes, tomorrowTimes) {
   let tomorrowTable = createWeatherTable(tomorrowTimes);
 
   // Insert everything
+  widget.appendChild(heading);
   widget.appendChild(todayHeading);
   widget.appendChild(todayTable);
   widget.appendChild(tomorrowHeading);
