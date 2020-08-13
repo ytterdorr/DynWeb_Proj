20f5102c3e598fc6ea3600b92690f43304507b64
Tore Haglund <tore.haglund@gmail.com>
2020-08-13 17:56:15 +0200

    menu flap on small screen on index page

--- js/left-aside.js
+++ js/left-aside.js
@@ -13,8 +13,26 @@ leftSide.innerHTML = `
     <a href="/html/english.html"><li>English</li></a>
   </ul>
 </nav>
+
   <footer>
     <p>©Djupvikshamn.se <br/>
     info@djupvikshamn.se</p>
   </footer>
 `;
+
+function toggleMenu() {
+  document.querySelector(".left-side").classList.toggle("show-menu");
+  document.querySelector(".menu-button").classList.toggle("show-menu");
+  document.querySelector(".close-menu-background").classList.toggle("hidden");
+}
+
+function showMenu() {
+  menu = document.querySelector(".left-side");
+  menu.classList.add("show-menu");
+  document.querySelector(".menu-button").classList.add("show-menu");
+}
+
+function hideMenu() {
+  document.querySelector(".left-side").classList.remove("show-menu");
+  document.querySelector(".menu-button").classList.remove("show-menu");
+}
